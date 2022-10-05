9de81ca4e0926c10bf1f10e52e56f20ec0a05789
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-10-05 09:50:16 +0900

    2022-10-05

--- src/CH01/C01_OutMethod.java
+++ src/CH01/C01_OutMethod.java
@@ -21,14 +21,18 @@ public class C01_OutMethod {
 		 * %f : 실수 출력
 		 * %c : 한문장 출력
 		 * %s : 문자열 출력
-		 * 
-		 * */
+		 */
 //		System.out.printf();
-		System.out.printf("%d %d %d \n",10,20,30);
-		System.out.printf("%f %f %f \n",10.1,20.1,30.1);
-		System.out.printf("%c %c %c \n",'A','B','C');
-		System.out.printf("%s %s %s \n","this si","String","Test");
-		System.out.printf("%d.%s : %d\n",1,"국어",100);
+//		System.out.printf("%d %d %d \n",10,20,30);
+//		System.out.printf("%f %f %f \n",10.1,20.1,30.1);
+//		System.out.printf("%c %c %c \n",'A','B','C');
+//		System.out.printf("%s %s %s \n","this si","String","Test");
+//		System.out.printf("%d.%s : %d\n",1,"국어",100);
+		
+		System.out.printf("%c%c\t\t%c%c\t\t%c%c\t\t%c%c\n",'N','O','이','름','나','이','주','소');
+		System.out.printf("%d\t\t%s\t %d\t\t%s\n",1,"김광수",35,"대구 ");
+		System.out.printf("%d\t\t%s\t %d\t\t%s\n",2,"이태수",55,"울산 ");
+		System.out.printf("%d\t\t%s\t %d\t\t%s\n",3,"신성아",25,"김해 ");
 	}
 
 }
